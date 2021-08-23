b9e7269147b8717b436fc9f5d95377c4592882e4
setiyonoag <saiya.adzim@gmail.com>
2021-08-23 09:23:16 +0700

    "add generic generator"

--- src/main/java/com/example/universitas/model/entity/JurusanEntity.java
+++ src/main/java/com/example/universitas/model/entity/JurusanEntity.java
@@ -1,6 +1,7 @@
 package com.example.universitas.model.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
@@ -17,6 +18,17 @@ public class JurusanEntity {
     @Column(name = "nama_jurusan")
     private String namaJurusan;
 
+    @Id
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "jur"),
+                    @Parameter(name = "initial_value", value = "1"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
     @Column(name = "kode_jurusan")
     private Long kodeJurusan;
 
