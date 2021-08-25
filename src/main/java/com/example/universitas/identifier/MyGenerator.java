60df2549ea48eefa33e75eb17be0df5875d6dd6c
setiyonoag <saiya.adzim@gmail.com>
2021-08-25 15:46:34 +0700

    update generator

--- src/main/java/com/example/universitas/identifier/MyGenerator.java
+++ src/main/java/com/example/universitas/identifier/MyGenerator.java
@@ -1,9 +1,5 @@
 package com.example.universitas.identifier;
 
-import java.io.Serializable;
-import java.util.Properties;
-import java.util.stream.Stream;
-
 import org.hibernate.HibernateException;
 import org.hibernate.MappingException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
@@ -12,15 +8,20 @@ import org.hibernate.id.IdentifierGenerator;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.type.Type;
 
+import java.io.Serializable;
+import java.util.Properties;
+import java.util.stream.Stream;
+
 public class MyGenerator implements IdentifierGenerator, Configurable {
 
     private String prefix;
-
     @Override
-    public Serializable generate(SharedSessionContractImplementor session, Object obj) throws HibernateException {
+    public Serializable generate(
+            SharedSessionContractImplementor session, Object obj)
+            throws HibernateException {
 
-        String query = String.format("select %s from %s",
-                session.getEntityPersister(obj.getClass().getName(), obj).getIdentifierPropertyName(),
+        String query = String.format("select %s from %s", session.getEntityPersister(obj.getClass().getName(), obj)
+                        .getIdentifierPropertyName(),
                 obj.getClass().getSimpleName());
 
         Stream<String> ids = session.createQuery(query).stream();
@@ -30,12 +31,12 @@ public class MyGenerator implements IdentifierGenerator, Configurable {
                 .max()
                 .orElse(0L);
 
-        return prefix + "-" + (max + 1);
+        return prefix + "-" + String.format("%03d",(max + 1));
     }
+
 
     @Override
     public void configure(Type type, Properties properties, ServiceRegistry serviceRegistry) throws MappingException {
         prefix = properties.getProperty("prefix");
     }
-
 }
